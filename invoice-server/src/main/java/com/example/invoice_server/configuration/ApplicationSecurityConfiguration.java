4bede5681ceaf6321e516d87b4ef3dc3b8dc7d38
rudkura <rud.kura@gmail.com>
2025-03-22 18:01:36 +0100

    remove unused imports

--- invoice-server/src/main/java/com/example/invoice_server/configuration/ApplicationSecurityConfiguration.java
+++ invoice-server/src/main/java/com/example/invoice_server/configuration/ApplicationSecurityConfiguration.java
@@ -2,12 +2,10 @@ package com.example.invoice_server.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
--- invoice-server/src/main/java/com/example/invoice_server/controller/advice/GlobalExceptionAdvice.java
+++ invoice-server/src/main/java/com/example/invoice_server/controller/advice/GlobalExceptionAdvice.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
